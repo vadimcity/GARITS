9e572521bcb2dd998cfbfb6ddb7a0deb1200da4a
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-02-23 15:20:47 +0000

    Update Admin.java

--- GARITS/src/Admin.java
+++ GARITS/src/Admin.java
@@ -1,21 +1,23 @@
-//public class Admin extends User {
-//    int x = 0;
-//
-//    
-//    public Admin(bool li){
-//      super.loggedin = li;
-//    }
-//    
-//
-//    public String createAccount(){
-//    }
-//    public String deleteAccount(){
-//    }
-//    public String databaseRestore(){
-//    }
-//    public String databaseBackup(){
-//    }
-//    public String alterAccount(){
-//    }
-//
-//}
+/*
+public class Admin extends User {
+   int x = 0;
+
+   
+   public Admin(bool li){
+     super.loggedin = li;
+   }
+   
+
+   public String createAccount(){
+   }
+   public String deleteAccount(){
+   }
+   public String databaseRestore(){
+   }
+   public String databaseBackup(){
+   }
+   public String alterAccount(){
+   }
+
+}
+*/
