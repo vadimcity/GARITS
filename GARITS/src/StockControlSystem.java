230f1ed230253debc637a61803cc3aefdc075a0d
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-02-23 15:24:20 +0000

    Update StockControlSystem.java

--- GARITS/src/StockControlSystem.java
+++ GARITS/src/StockControlSystem.java
@@ -1,29 +1,31 @@
-//public class StockControlSystem {
-//    int x = 0;
-//
-//    public StockControlSystem(){
-//    }
-//
-//
-//    public String stockAlertPopUp(){
-//    }
-//    public String orderMoreParts(){
-//    }
-//
-//    public String orderParts(){
-//    }
-//    public String stockLevelReport(){
-//    }
-//
-//    public String selectPart(){
-//    }
-//    public String sellSpareParts(){
-//    }
-//    public String getParts(){
-//    }
-//    public String updateStockLevel(){
-//    }
-//    public String changePrice(){
-//    }
-//
-//}
+/*
+public class StockControlSystem {
+   int x = 0;
+
+   public StockControlSystem(){
+   }
+
+
+   public String stockAlertPopUp(){
+   }
+   public String orderMoreParts(){
+   }
+
+   public String orderParts(){
+   }
+   public String stockLevelReport(){
+   }
+
+   public String selectPart(){
+   }
+   public String sellSpareParts(){
+   }
+   public String getParts(){
+   }
+   public String updateStockLevel(){
+   }
+   public String changePrice(){
+   }
+
+}
+*/
