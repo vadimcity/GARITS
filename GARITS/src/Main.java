c641962dc680aba7caeacd1b499845aab4ea31e5
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-02-25 12:27:53 +0000

    Update Main.java

--- GARITS/src/Main.java
+++ GARITS/src/Main.java
@@ -1,8 +1,15 @@
-//public class Main {
-//    class main {
-//        public static void main(String[] args) {
-//            CreateCustomer c = new CreateCustomer();
-//            //c.start();
-//        }
-//    }
-//}
+public class Main {
+    public static void main(String[] args) {
+        test();
+
+        //c.start();
+    }
+
+    public static void test(){
+        Admin a = new Admin("Sampson", "password");
+        Foreperson fo = new Foreperson("Sampson", "password");
+        Franchisee fr = new Franchisee("Sampson", "password");
+        Mechanic m = new Mechanic("Sampson", "password");
+        Receptionist r = new Receptionist("Sampson", "password");
+    }
+}
