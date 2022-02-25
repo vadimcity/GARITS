e0cb8b5c5560d741f39032704cb4c1176f523eb4
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-02-25 12:27:33 +0000

    Update Franchisee.java

--- GARITS/src/Franchisee.java
+++ GARITS/src/Franchisee.java
@@ -1,27 +1,22 @@
-/*
 public class Franchisee extends Foreperson {
-   int x = 0;
+    int x = 0;
+
+    public Franchisee(String un, String pw){
+        super(un,pw);
+    }
 
 
-   public Admin(boolean li, string un, string pw){
-     super.loggedin = li;
-     super.username = un;
-     super.password = pw;
-   }
+    public void setDiscountPlan(){
+    }
+    public void alterDiscountPlan(){
+    }
 
+    public void createAccountHolder(){
+    }
+    public void payLateOption(){
+    }
 
-   public String setDiscountPlan(){
-   }
-   public String alterDiscountPlan(){
-   }
-   
-   public String createAccountHolder(){
-   }
-   public String payLateOption(){
-   }
-   
-   public String alterAccountHolder(){
-   }
+    public void alterAccountHolder(){
+    }
 
 }
-*/
