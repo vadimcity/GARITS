6b912f496dbd0fc5772ddcaaefa65b2574aa7337
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-02-26 18:25:45 +0000

    Update Franchisee.java

--- GARITS/src/Franchisee.java
+++ GARITS/src/Franchisee.java
@@ -1,12 +1,14 @@
 public class Franchisee extends Foreperson {
     int x = 0;
 
-    public Franchisee(String un, String pw){
-        super(un,pw);
+    public Franchisee(String un, String pw, StockControlSystem scs){
+        super("Franchisee", un, pw, scs);
     }
 
 
-    public void setDiscountPlan(){
+    public void setDiscountPlan(String dp, int CustID){
+        //can be fixed, variable or flexible
+        DatabaseConnection.databaseAffectTemplate("UPDATE customermemberlist SET Discountplan='" + dp + "' WHERE ID='" + CustID + "'");
     }
     public void alterDiscountPlan(){
     }
