e516921a2a0a8c3f31308a51033ebff4f9b2ff72
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-02-26 14:32:51 +0000

    Update DatabaseConnection.java

--- GARITS/src/DatabaseConnection.java
+++ GARITS/src/DatabaseConnection.java
@@ -1,25 +1,32 @@
-// import java.sql.Connection;
-// import java.sql.DriverManager;
-// import java.sql.SQLException;
-// import java.sql.Statement;
-// 
-// public class DatabaseConnection {
-//    static final String database_url = "jdbc:mysql://192.168.0.10:3308";
-//    static final String user = "root";
-//    static final String password = "example";
-// 
-//    public static void main(String[] args) {
-//       // Open a connection
-//       try(Connection conn = DriverManager.getConnection(database_url, user, password);
-//           Statement stmt = conn.createStatement();
-//       ) {
-//          // Execute a query
-//          System.out.println("Inserting records into the table...");
-//          String sql = INSERT "INTO CustomerAccountHolder VALUES (100, 'Zara', 'Ali', 18)";
-//          stmt.executeUpdate(sql);
-//          System.out.println("Inserted records into the table...");
-//       } catch (SQLException e) {
-//          e.printStackTrace();
-//       }
-//    }
-// }
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    static final String database_url = "jdbc:mysql://localhost:3306/t18database";
+    static final String mysqlUser = "root";
+    static final String mysqlPassword = "jack123";
+
+    //send a query to the database in the form of the string.
+    public static void databaseAffectTemplate(String sql){
+        try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
+            Statement stmt = conn.createStatement();
+        ) {
+            // Execute a query
+            System.out.println("Altering the table...");
+            stmt.executeUpdate(sql);
+            System.out.println("Altered the table...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    //send a query to the database and get a boolean in return
+    public static boolean databaseReturnBool(String sql){ }
+
+    //send a query to the database and get a String in return
+    public static String databaseReturnString(String sql){ }
+    */
+}
