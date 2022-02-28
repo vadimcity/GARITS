bcba56cfc08be79826dfbe8781ecbe4bb89577f3
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-02-28 13:57:15 +0000

    Update DatabaseConnection.java

--- GARITS/src/DatabaseConnection.java
+++ GARITS/src/DatabaseConnection.java
@@ -3,13 +3,57 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//an example of a database query would be
+//String sql = "INSERT INTO useraccounts VALUES ('Samantha', 'x123', 'Mechanic')";
+
 public class DatabaseConnection {
     static final String database_url = "jdbc:mysql://localhost:3306/t18database";
     static final String mysqlUser = "root";
     static final String mysqlPassword = "jack123";
 
+/*
+    public static void main(String[] args) {
+        // Open a connection
+
+//        try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
+//            Statement stmt = conn.createStatement();
+//        ) {
+//            // Execute a query
+//            System.out.println("Inserting records into the table...");
+//            String sql = "INSERT INTO useraccounts VALUES ('Samantha')";
+//            stmt.executeUpdate(sql);
+//            System.out.println("Inserted records into the table...");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+        userAccountsInsert();
+    }
+
+    public static void userAccountsInsert(){
+        try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
+            Statement stmt = conn.createStatement();
+        ) {
+            // Execute a query
+            System.out.println("Inserting records into the table...");
+            String sql = "INSERT INTO useraccounts VALUES ('Solomon', 'x123', 'Receptionist')";
+            stmt.executeUpdate(sql);
+            System.out.println("Inserted records into the table...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+ */
+
+
     //send a query to the database in the form of the string.
     public static void databaseAffectTemplate(String sql){
+
         try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
             Statement stmt = conn.createStatement();
         ) {
@@ -22,10 +66,6 @@ public class DatabaseConnection {
         }
     }
 
-    
-    //send a query to the database and get a boolean in return
-    //public static boolean databaseReturnBool(String sql){ }
-    
     //send a query to the database and get a boolean in return
     //public static boolean databaseReturnBool(String sql){
     public static void databaseReturnBool(){
@@ -49,8 +89,7 @@ public class DatabaseConnection {
         }
     }
 
-    /*
     //send a query to the database and get a String in return
-    public static String databaseReturnString(String sql){ }
-    */
+    //public static String databaseReturnString(String sql){ }
+
 }
