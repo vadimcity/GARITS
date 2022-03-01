5acfb30fc769cc7ddc501548c85c87b1b9f76e37
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-03-01 00:06:46 +0000

    Update DatabaseConnection.java

--- GARITS/src/DatabaseConnection.java
+++ GARITS/src/DatabaseConnection.java
@@ -1,95 +1,70 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
-//an example of a database query would be
-//String sql = "INSERT INTO useraccounts VALUES ('Samantha', 'x123', 'Mechanic')";
+import java.io.*;
 
 public class DatabaseConnection {
     static final String database_url = "jdbc:mysql://localhost:3306/t18database";
     static final String mysqlUser = "root";
     static final String mysqlPassword = "jack123";
 
-/*
-    public static void main(String[] args) {
-        // Open a connection
-
-//        try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
-//            Statement stmt = conn.createStatement();
-//        ) {
-//            // Execute a query
-//            System.out.println("Inserting records into the table...");
-//            String sql = "INSERT INTO useraccounts VALUES ('Samantha')";
-//            stmt.executeUpdate(sql);
-//            System.out.println("Inserted records into the table...");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-
-        userAccountsInsert();
-    }
-
-    public static void userAccountsInsert(){
-        try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
-            Statement stmt = conn.createStatement();
-        ) {
-            // Execute a query
-            System.out.println("Inserting records into the table...");
-            String sql = "INSERT INTO useraccounts VALUES ('Solomon', 'x123', 'Receptionist')";
-            stmt.executeUpdate(sql);
-            System.out.println("Inserted records into the table...");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
- */
-
-
     //send a query to the database in the form of the string.
     public static void databaseAffectTemplate(String sql){
-
         try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
             Statement stmt = conn.createStatement();
         ) {
             // Execute a query
             System.out.println("Altering the table...");
-            stmt.executeUpdate(sql);
+            int result=stmt.executeUpdate(sql);
+            System.out.println(result+" records affected");
             System.out.println("Altered the table...");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    //send a query to the database and get a boolean in return
-    //public static boolean databaseReturnBool(String sql){
-    public static void databaseReturnBool(){
-        String query = "SELECT * FROM useraccounts";
+    //public static void databaseReturnString(String sql, int column){
+    public static ArrayList<String> databaseReturnString(String sql, String column){
+        ArrayList<String> al = new ArrayList<>();
 
-        try (Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
-             PreparedStatement pst = conn.prepareStatement(query);
-             ResultSet rs = pst.executeQuery()) {
+        try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
+            Statement stmt = conn.createStatement();
+        ) {
+            // Execute a query
+            //System.out.println("Reading the table...");
+            ResultSet rs=stmt.executeQuery(sql);
+            //System.out.println("Table read...");
 
-            while (rs.next()) {
-
-                System.out.print(rs.getInt(1));
-                System.out.print(": ");
-                System.out.println(rs.getString(2));
+            while(rs.next()) {
+                String s = rs.getString(column);
+                //System.out.println(s);
+                al.add(s);
             }
-
-        } catch (SQLException ex) {
-
-            Logger lgr = Logger.getLogger(DatabaseConnection.class.getName());
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return al;
     }
+    public static ArrayList<Integer> databaseReturnInt(String sql, String column){
+        ArrayList<Integer> al = new ArrayList<>();
 
-    //send a query to the database and get a String in return
-    //public static String databaseReturnString(String sql){ }
+        try(Connection conn = DriverManager.getConnection(database_url, mysqlUser, mysqlPassword);
+            Statement stmt = conn.createStatement();
+        ) {
+            // Execute a query
+            //System.out.println("Reading the table...");
+            ResultSet rs=stmt.executeQuery(sql);
+            //System.out.println("Table read...");
 
+            while(rs.next()) {
+                Integer i = rs.getInt(column);
+                //System.out.println(i);
+                al.add(i);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return al;
+    }
 }
