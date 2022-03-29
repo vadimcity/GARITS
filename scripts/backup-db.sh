5f4a4022495042626b9e1a10e3001674c6910967
vadimcity <vadims.pasinicevs@city.ac.uk>
2022-03-29 17:45:13 +0100

    Added ping script.

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 date_var=$(date +"%m-%d-%Y-%T")
-mysqldump --host=192.168.0.10 -P 3308 -u $1 -p$2 t18database > $PWD/backups/dbbackup-$date_var.sql 
+mysqldump --host=192.168.0.10 -P 3308 -u $1 -p$2 t18database > ./dbbackup-$date_var.sql
 RESULT=$?
 if [ $RESULT == 0 ]; then 
-	echo "dbbackup-$date_var.sql has been backed."
+	echo "Back up Date: $date_var has been made."
 	echo "dbbackup-$date_var.sql" >> $PWD/backups/restore-point.txt
 else
 	echo "Back up failed. Check Username and Password."
