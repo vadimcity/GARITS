192ec66ae92851d0830e81eb3dc3425a2ae94635
J4ckM <71776595+J4ckM@users.noreply.github.com>
2022-03-30 12:28:51 +0100

    Update ForepersonPanel.java

--- GARITS/src/GUI/ForepersonPanel.java
+++ GARITS/src/GUI/ForepersonPanel.java
@@ -16,4 +16,29 @@ public class ForepersonPanel {
     private JButton viewPendingJobListButton;
     private JButton receptionistButton;
     private JButton mechanicButton;
+    
+    int x = 0;
+
+    public Foreperson(String un, String pw, StockControlSystem scs){
+        super("Roles.Foreperson", un,pw, scs);
+    }
+    public Foreperson(String role, String un, String pw, StockControlSystem scs){
+        super(role, un,pw, scs);
+    }
+
+
+    public void allocateMechanic(int JobID, String Mechanic, String time){
+        int CustID; String details;
+        //first read info of JobID in pendingjoblist, and copy them to CustID and details.          So for now we'll initialise them ourselves
+
+        CustID = DatabaseConnection.databaseReturnIndivInt(
+                "SELECT * FROM pendingjoblist WHERE JobID=" + JobID, "CustomerID");
+        details = DatabaseConnection.databaseReturnIndivString(
+                "SELECT * FROM pendingjoblist WHERE JobID=" + JobID, "Details");
+
+        DatabaseConnection.databaseAffectTemplate("DELETE FROM pendingjoblist WHERE JobID=" + JobID);
+        DatabaseConnection.databaseAffectTemplate("INSERT INTO activejoblist VALUES ('" + JobID + "', '" + CustID + "', '" + time + "', '" + Mechanic + "', '" + details + "')");
+    }
+    public void addJobToPendingList(){
+    }
 }
